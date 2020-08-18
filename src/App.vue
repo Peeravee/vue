44133ac113731d102ee9c7287ac97ec933a690b6
Peeravee <68525360+Peeravee@users.noreply.github.com>
2020-08-18 14:15:33 +0700

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+   <!--<br />   <img alt="bg" src="./assets/12.jpg" /><br />-->
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{button}}</h1>
+    
+      
+
+      <div class="col-3"><counter lable="Start" @cc="sum" /></div> 
+      <div class="col-3"><counter lable="Attack" @cc="sum" /></div>
+      <div class="col-3"><counter lable="Specia" @cc="sum" /></div>
+      <div class="col-3"><counter lable=" Attack " @cc="sum" /></div>
+ 
+
+
+
+
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import Counter from "./components/Counter.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    Counter,
+  },
+  data: function(){
+    return{
+      total: 0,
+      name:'',
+      code:'',
+      gpa:'0.0',
   }
-}
+  },
+methods : {
+  // 3. 
+  sum(counterValue){
+    this.total += counterValue
+   
+  },
+
+},
+
+};
 </script>
 
 <style>
+body{
+  background-image: url(https://blogs-images.forbes.com/erikkain/files/2019/05/endgame-1.jpg)
+  
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Counter.vue
@@ -0,0 +1,50 @@
+<template>
+<div>
+  <div id="example-1">
+  <button @click="upFunction" class="btn btn-primary">{{lable}}</button>
+
+  
+ 
+
+</div>
+</div>
+</template>
+
+<script>
+export default {
+ data: function (){
+     return{
+    counter: 0,
+    fsize:50
+    }
+  },
+  props: {
+    lable: String
+  },
+  methods: {
+    upFunction: function () {
+     this.counter+=1
+      this.fsize+=10
+
+     console.log(this.fsize);
+      console.log(this.counter);
+
+      // ส่งค่าตัวแปร counter ออกไปผ่านตัวแปร cc
+      this.$emit('cc', this.counter)
+
+
+   },
+    downFunction: function () {
+     this.counter-=1
+      this.fsize-=10
+
+      console.log(this.fsize);
+      console.log(this.counter);
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
